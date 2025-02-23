8d89fff0f0a0a63a93322d32f01067a65c6b44d0
Pranav Karra <pranavkarra001@gmail.com>
2025-02-23 12:43:42 -0500

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export const metadata: Metadata = {
     siteName: 'ML@PSU',
     images: [
       {
-        url: '/preview.png',
-        width: 1200,
-        height: 630,
+        url: '/preview-square.png',
+        width: 256,
+        height: 256,
         alt: 'ML@PSU - Machine Learning Club at Penn State University'
       }
     ],
@@ -26,13 +26,14 @@ export const metadata: Metadata = {
     type: 'website',
   },
   twitter: {
-    card: 'summary_large_image',
+    card: 'summary',
     title: 'ML@PSU',
     description: 'Machine Learning Club at Penn State University',
-    images: ['/preview.png'],
+    images: ['/preview-square.png'],
   },
   other: {
-    'og:image:secure_url': 'https://mlpsu.org/preview.png',
+    'og:image:type': 'image/png',
+    'og:image:secure_url': 'https://mlpsu.org/preview-square.png',
   }
 }
 
